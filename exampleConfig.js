71cdfea79d9b0eeaa100a2d591084eac016cc2b3
georgecronjepsg <52321112+georgecronjepsg@users.noreply.github.com>
2019-06-28 15:36:01 +0200

    Add logger backend to backends config

--- exampleConfig.js
+++ exampleConfig.js
@@ -122,6 +122,6 @@ Optional Variables:
   graphitePort: 2003
 , graphiteHost: "graphite.example.com"
 , port: 8125
-, backends: ["./backends/console", "./backends/graphite" ]
+, backends: ["./backends/logger", "./backends/console", "./backends/graphite" ]
  , servers: [ "./servers/tcp" ]
 }
